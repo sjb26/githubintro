f81c8c18fd3b1aefc3466647bdc7bb8926edd402
sjb26 <78023743+sjb26@users.noreply.github.com>
2021-05-05 23:11:43 +0100

    update comments

--- assigment1_abm/agentframework.py
+++ assigment1_abm/agentframework.py
@@ -7,11 +7,12 @@ class Agent:
 
         self.environment = environment #used as food for agents to eat in def eat
         self.store = 0                 #creates the food stores for the agents to use in def eat
-        self.agents = agents           #used for the sharing_with_neighbour function and 
+        self.agents = agents           #used for the sharing_with_neighbour function 
         self.record = 0                #create record for how many times an agent is sick
         self.count = 0                 #create count for how many times an agent moves
         
-        # DOES NOT WORK AS INTENDED. If the number of agents specified in the variables exceeds the number of td_ys/td_xs pairs, the code will complain that the 'list index is out of range'
+        # DOES NOT WORK AS INTENDED. If the number of agents specified in the variables exceeds the number of td_ys/td_xs pairs, 
+        # the code will complain that the 'list index is out of range'
         if (y == None):
             self.y = random.randint(0,100)
         else:
@@ -22,7 +23,7 @@ class Agent:
             self.x = x
         
         
-    def move(self):
+    def move(self):  # rules for moving
         if random.random() < 0.5:
             self.y = (self.y + 1) % 300 #if random number is <0.05, add 1 to the y-axis of the agent
             self.count += 1             #counts how many times the agent has moved
@@ -42,7 +43,7 @@ class Agent:
         return "%s, is at coordinates x: %s, y: %s, has a store of %s, and has been sick %s time/times" % (self.count, self.x, self.y, self.store, self.record)
 
     
-    def eat(self): # can you make it eat what is left?
+    def eat(self): # rules for eating
         if self.environment[self.y][self.x] > 100:    #if the environment has more than 100
             self.environment[self.y][self.x] -= 100   #take 100 from the environment
             self.store += 100                         #add 100 to agent store
@@ -69,7 +70,8 @@ class Agent:
                 ave = round(sum/ 2)             #divide and average the stores. Round used so no decimal places occur in the animation
                 self.store = ave                #agents have the same averaged food stores
                 agent.store = ave 
-                print("Agent after move " + str(agent) + " and is sharing " + str(ave) + " from a distance of " + str(rdist))                     #print what amount was shared and at what distance everytime this occurs
+                # print what amount was shared and at what distance everytime this occurs
+                print("Agent after move " + str(agent) + " and is sharing " + str(ave) + " from a distance of " + str(rdist))
     
 class  Wolf:
 	#wolf constructor
@@ -77,7 +79,9 @@ class  Wolf:
         self.y = random.randint(0, 99)       #randomly select a starting position from 0-99 for y.axis
         self.x = random.randint(0, 99)       #randomly selects a starting position from 0-99 for x.axis
         self.store = 0                       #creates the food stores for the wolf to use in def eat_agents
-        self.count = 0                       #creates the count for the wolf to use in def eat_agents when a sheep is eaten. Also used when annotating the model display how many sheep each wolf has eaten.
+        self.count = 0                       #creates the count for the wolf to use in def eat_agents when a sheep is eaten.
+                                             #Also used when annotating the model display how many sheep each wolf has eaten.
+                    
             
     def move(self):
     # wolves move up to 3 places per iteration
