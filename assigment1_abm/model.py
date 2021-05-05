24aaef933e369693bd53e1996c9d2a3d04238ec8
sjb26 <78023743+sjb26@users.noreply.github.com>
2021-05-05 22:32:23 +0100

    update

--- assigment1_abm/model.py
+++ assigment1_abm/model.py
@@ -40,9 +40,9 @@ environment = []
 
 ## Define variables
 ## Define how many sheep in model
-num_of_agents = 20
+num_of_agents = 10
 ## Define how many wolves in model
-num_of_wolves = 2
+num_of_wolves = 20
 ## Define how many iterations - feeds into how many often frames are shown in the GUI
 num_of_iterations = 1
 ## Define distance at which agents interact with each other
